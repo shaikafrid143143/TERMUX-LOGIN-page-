012de9245b75002ce25439f3b8e6cbabd10dbae1
shaikafrid143143 <70587992+shaikafrid143143@users.noreply.github.com>
2021-04-20 09:16:40 +0530

    Update AFRID.py

--- AFRID.py
+++ AFRID.py
@@ -1,1 +1,14 @@
-print ("CODED  BY SHAIK AFRID:)
+imort colorama 
+from colorama import From,Back,Style
+import time 
+print(time.sleep(5)+"CODED BY SHAIK AFRID")
+time.sleep(5)
+print(Fore.YELLOW + Style.BRIGHt+Back.WHITE+UR NOT A CODERBRESPECT CODERS)
+time.sleep(3)
+X=INPUT(Fore.RED+Style.BRIGHT+"ENTER USERNAME :-)
+Y=INPUT(time.sleep(2)+Fore.RED+Style.BRIGHT+"ENTER PASSWORD :-)
+Z=INPUT(time.sleep(3)+Fore.RED+Style.BRIGHT+"REPET PASSWORD :-)
+time.slleep(6)
+if Y!=Z:
+        print(Fore.PINK+Style.BRIGHT+"PASSWORD DONT MATCH")
+
